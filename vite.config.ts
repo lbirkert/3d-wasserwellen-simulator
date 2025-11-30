9e73a3a9a4fa67f4e272f7d655faefd183501252
Lucas Birkert <lucasbirkert@gmail.com>
2025-11-30 19:55:52 +0100

    fix: base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const basePath = process.env.BASE_PATH || '/';
+    
     return {
+      base: basePath,
       server: {
         port: 3000,
         host: '0.0.0.0',
