e9a59b9a6cf974d6ac908425d065c901c998b792
Lucas Birkert <lucasbirkert@gmail.com>
2025-12-18 09:34:38 +0100

    feat: add app

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,28 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    const basePath = process.env.BASE_PATH || undefined;
-    
-    return {
-      base: basePath,
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
+  const env = loadEnv(mode, '.', '');
+  const basePath = process.env.BASE_PATH || undefined;
+
+  return {
+    base: basePath,
+    build: {
+      outDir: 'app/src', // Change to whatever folder name you want
+      emptyOutDir: true, // Clean the output directory before building
+    },
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    define: {
       //  'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       //  'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
